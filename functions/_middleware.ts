90f6a01148a5cb1a1dc9d710965f9bc264071d84
Jeremy Bunting <187095+qbunt@users.noreply.github.com>
2025-01-06 01:14:52 -0500

    dropping old stuff

--- functions/_middleware.ts
+++
@@ -1,5 +0,0 @@
-import sentryPlugin from "@cloudflare/pages-plugin-sentry";
-
-export const onRequest: PagesFunction = sentryPlugin({
-  dsn: "https://b51183f2032048e79b7a359d072bbe78@o80441.ingest.sentry.io/6421858",
-});
